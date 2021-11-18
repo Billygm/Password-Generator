0553ba21e63067b7d92ace29c57ba579173f308c
Billy Mott <billygm@uw.edu>
2021-11-18 10:50:24 -0800

    got my password to generate as numbers representing the place that character is in the string

--- script.js
+++ script.js
@@ -4,9 +4,66 @@ var generateBtn = document.querySelector("#generate");
 // Character Types
 var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var numbers = ["0","1","2","3","4","5","6","7","8","9"];
-var spCharacters = ["`","~","!","@","#","$","%","^","&","*","(",")","-","_","=","+","|","}","]","{","[","'",";",":","/","?",".",">","<",","];
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var spCharacters = ["`", "~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "|", "}", "]", "{", "[", "'", ";", ":", "/", "?", ".", ">", "<", ","];
+var none = [""]
+var characters = [];
 
+
+var generatePassword = function() {
+
+  // ask user for password criteria
+  var pass = [""];
+  var pwLength =  8 // prompt("Choose your password length, enter a number between 8 - 128.");
+
+  if (!pwLength) {
+    return pass;
+  } else if (pwLength > 128 || pwLength < 8) {
+    alert("Password length must be between 8 and 128 characters.");
+  } else if (pwLength >= 8 && pwLength <= 128) {
+    var ucLetters = true; // confirm("Would you like to include uppercase letters?");
+    var lcLetters = true; // confirm("Would you like to include lowercase letters?");
+    var num = true; // confirm("Would you like to include numbers?");
+    var spChar = true; // confirm("Would you like to include special characters?");
+  }
+  
+  if (ucLetters === false && lcLetters === false && num === false && spChar === false) {
+    alert("You must choose at least one character type.");
+  }
+  
+  // Pulling characters for password
+  if (ucLetters) {
+    pass += Math.floor(Math.random() * uppercase.length);
+    console.log(pass, uppercase[pass]);
+    characters = characters.concat(uppercase);
+  }
+
+  if (lcLetters) {
+    pass += Math.floor(Math.random() * lowercase.length);
+    console.log(pass, lowercase[pass]);
+    characters = characters.concat(lowercase);
+  }
+
+  if (num) {
+    pass += Math.floor(Math.random() * numbers.length);
+    console.log(pass, numbers[pass]);
+    characters = characters.concat(numbers)
+  }
+
+  if (spChar) {
+    pass += Math.floor(Math.random() * spCharacters.length);
+    console.log(pass, spCharacters[pass]);
+    characters = characters.concat(spCharacters)
+  }
+  console.log(characters)
+
+  for (var i=pass.length; i <= pwLength; i++) {
+    pass += Math.floor(Math.random() * characters.length);
+    console.log(pass, characters[pass]);
+  }
+  
+return pass;
+}
 
 // Write password to the #password input
 function writePassword() {
@@ -19,22 +76,3 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-var generatePassword = function() {
-  // ask user for password criteria
-  var pwLength = window.prompt("Enter Password length, " + 8 + "-" + 128 + " charachters");
-
-  if (!pwLength) {
-    return;
-  }
-  else if (pwLength < 8 && pwLength > 128) {
-    alert("Password length must be between 8 and 128 characters.")
-    console.log(pwLength);
-  }
-  else if (pwLength > 7 && pwLength < 129) {
-    var ucLetters = confirm("Would you like to include uppercase letters?");
-    var lcLetters = confirm("Would you like to include lowercase letters?");
-    var num = confirm("Would you like to include numbers?");
-    var SpChar = confirm("Would you like to include special characters?");
-  }
-}
